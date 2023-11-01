4f34cd417591fbe183002125f7ee853628c51ea0
mk010101 <max@nanana.co.uk>
2023-11-01 22:44:39 +0000

    typeface

--- src/routes/link/+page.svelte
+++ src/routes/link/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	let heading = 'h1';
 	let weight = '600';
 </script>
 
-<h2 class="up" style="font-family:{heading}; font-weight:{weight};">
+<h2 class="up" style="font-weight:{weight};">
 	Whereas recognition of the inherent dignity 1234567890.
 </h2>
 <p>
@@ -14,9 +13,7 @@
 	attacks upon his honour and reputation. Everyone has the right to the protection of the law
 	against such interference or attacks. Gas giants. Just this. In the light of the Sun.
 </p>
-<h2 style="font-family:{heading}; font-weight:{weight};">
-	Whereas recognition of the inherent dignity 1234567890.
-</h2>
+<h3 style="font-weight:{weight};">Whereas recognition of the inherent dignity 1234567890.</h3>
 <p>
 	No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full
 	equality to a fair and public hearing by an independent and impartial tribunal, in the
@@ -26,27 +23,13 @@
 	against such interference or attacks. Gas giants. Just this. In the light of the Sun.
 </p>
 <p>
-	<button class="btn" variant="primary" style="font-family: {heading};">Find out more</button>
-	<button class="btn" style="font-family: {heading};"
-		><span>Something else</span><span class="dot" /></button
-	>
-</p>
-<p>
-	<select bind:value={heading}>
-		<option value="h1">h1</option>
-		<option value="h2">h2</option>
-	</select>
-
-	<input type="range" min="100" max="900" step="10" bind:value={weight} />
-	<span>{weight}</span>
+	<button class="btn" variant="primary">Find out more</button>
+	<button class="btn"><span>Something else £45.92</span><span class="dot" /></button>
 </p>
 
 <style>
-	:root {
-		--header: h1;
-	}
 	h2 {
-		font-family: var(--header);
+		font-family: var(--font-family-header);
 		font-size: 2.5rem;
 		font-optical-sizing: auto;
 		font-weight: 600;
@@ -55,19 +38,24 @@
 		margin-bottom: 1rem;
 		color: #2e2e2e;
 	}
+	h3 {
+		font-size: 2rem;
+		font-style: italic;
+		line-height: 1.15;
+	}
 
 	h2.up {
 		text-transform: uppercase;
 	}
 
 	p {
-		font-family: t1;
 		margin-bottom: 2.5rem;
 	}
 
 	button.btn {
-		ztext-transform: uppercase;
-		zfont-weight: 600;
+		zfont-family: var(--font-family-body);
+		text-transform: uppercase;
+		font-weight: 600;
 		font-size: 1.25rem;
 		margin-right: 1rem;
 		line-height: 1rem;
