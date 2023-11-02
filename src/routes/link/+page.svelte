f1f2c58acf8a4cbc39ea628a26fe88e39a65c2c9
mk010101 <max@nanana.co.uk>
2023-11-02 20:23:54 +0000

    vertical rythm

--- src/routes/link/+page.svelte
+++ src/routes/link/+page.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
-	let weight = '600';
+	let weight = '700';
 </script>
 
-<h2 class="up" style="font-weight:{weight};">
+<!-- <p>
+	<input type="range" bind:value={weight} min="100" max="900" step="10" />
+	<span>{weight}</span>
+</p> -->
+
+<h1 style="zfont-weight:{weight};">Everyone is entitled to something special</h1>
+<p>
+	No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full
+	equality to a fair and public hearing by an independent and impartial tribunal, in the
+	determination of his rights and obligations and of any criminal charge against him. No one shall
+	be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to
+	attacks upon his honour and reputation. Everyone has the right to the protection of the law
+	against such interference or attacks. Gas giants. Just this. In the light of the Sun.
+</p>
+<p>
+	No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full
+	equality to a fair and public hearing by an independent and impartial tribunal, in the
+	determination of his rights and obligations and of any criminal charge against him. No one shall
+	be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to
+	attacks upon his honour and reputation. Everyone has the right to the protection of the law
+	against such interference or attacks. Gas giants. Just this. In the light of the Sun.
+</p>
+<h2 class="up" style="zfont-weight:{weight};">
 	Whereas recognition of the inherent dignity 1234567890.
 </h2>
 <p>
@@ -13,7 +35,7 @@
 	attacks upon his honour and reputation. Everyone has the right to the protection of the law
 	against such interference or attacks. Gas giants. Just this. In the light of the Sun.
 </p>
-<h3 style="font-weight:{weight};">Whereas recognition of the inherent dignity 1234567890.</h3>
+<h3 style="zfont-weight:{weight};">Whereas recognition of the inherent dignity 1234567890.</h3>
 <p>
 	No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full
 	equality to a fair and public hearing by an independent and impartial tribunal, in the
@@ -27,54 +49,18 @@
 	<button class="btn"><span>Something else £45.92</span><span class="dot" /></button>
 </p>
 <p>
-	<input type="range" bind:value={weight} min="100" max="900" />
-	<span>{weight}</span>
+	No one shall be subjected to arbitrary arrest, detention or exile. Everyone is entitled in full
+	equality to a fair and public hearing by an independent and impartial tribunal, in the
+	determination of his rights and obligations and of any criminal charge against him. No one shall
+	be subjected to arbitrary interference with his privacy, family, home or correspondence, nor to
+	attacks upon his honour and reputation. Everyone has the right to the protection of the law
+	against such interference or attacks. Gas giants. Just this. In the light of the Sun.
 </p>
 
 <style>
-	h2,
-	h3 {
-		color: rgb(93, 93, 93);
-		color: var(--color-primary-2);
-		font-family: FHeader, Georgia, 'Times New Roman', Times, serif;
-	}
-	h2 {
-		zfont-family: var(--font-family-header);
-		font-size: 2.5rem;
-		font-optical-sizing: auto;
-		font-weight: 600;
-		/* font-style: italic; */
-		line-height: 1;
-		margin-bottom: 0.25rem;
-	}
-	h3 {
-		font-size: 2rem;
-		font-style: italic;
-		line-height: 1.15;
-		letter-spacing: -0.01ch;
-	}
-
-	h2.up {
-		text-transform: uppercase;
-	}
-
-	p {
-		margin-bottom: 2.5rem;
-	}
-
-	button.btn {
-		zfont-family: var(--font-family-body);
-		text-transform: uppercase;
-		font-weight: 600;
-		font-size: 1.25rem;
-		margin-right: 1rem;
-		zline-height: 1rem;
-		letter-spacing: 0.05rem;
-	}
-
-	.dot {
-		width: 1rem;
-		height: 0.1rem;
-		background-color: #666;
+	.spacer {
+		width: 1.75rem;
+		height: 1.75rem;
+		background-color: rgba(206, 6, 6, 0.5);
 	}
 </style>
